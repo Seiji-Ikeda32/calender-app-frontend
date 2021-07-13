7dca7387501fe74932528403b818825b35068bfa
池田聖示 <ikeda-seiji@ikedakiyoshishimesenoMacBook-Pro-2.local>
2021-07-13 22:15:12 +0900

    add axios

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld />
+  <div id="app">
+    <Calendar />
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Calendar from './components/Calendar.vue';
 
 export default {
-  name: "App",
+  name: 'App',
   components: {
-    HelloWorld,
+    Calendar,
   },
 };
 </script>
-
